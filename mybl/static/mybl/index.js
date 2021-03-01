a9521dde2653da29094c084c3ab5430b4345de09
ksn38 <ksn38@yandex.ru>
2021-03-01 11:54:38 +0300

    -

--- mybl/static/mybl/index.js
+++ mybl/static/mybl/index.js
@@ -17,7 +17,7 @@ let dict = function (dif) {
     dateLast.setDate(dateLast.getDate() - 2);
   };
   dateLast = dateLast.toISOString().slice(0, 10);
-  console.log(`dateLast slice ${dateLast}`);
+  //console.log(`dateLast slice ${dateLast}`);
   for (let i = 0; i < 201; i += 100) {
     let url = 'https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/tqbr/securities.json?date=' + dateLast + '&start=' + i;
     $.ajax({
@@ -34,9 +34,21 @@ let dict = function (dif) {
   };
   return day;
 };
-  
+
 
 button.onclick = function () {
+  for (let i of namesHigh) {
+    if (i.classList.length > 1){
+    i.classList.remove('bg-primary');
+    };
+  };
+  
+    for (let i of namesLow) {
+    if (i.classList.length > 1){
+    i.classList.remove('bg-primary');
+    };
+  };
+  
   let date = new Date();
   if (date.getDay() == 1 && parseInt(days.value) < 4) {
     days.value = 4;
@@ -48,6 +60,7 @@ button.onclick = function () {
   let dict2 = new Map([...dict(days.value).entries()]);
   let listKeys = [...dict1.keys()];
   let outMap = new Map();
+  
   for (let key = 0; key < listKeys.length; key++) {
     let val = ((dict1.get(listKeys[key])/dict2.get(listKeys[key])) - 1) * 10000;
     if (!Number.isNaN(val)) {
@@ -56,16 +69,24 @@ button.onclick = function () {
       outMap.set(listKeys[key], val);
     };
   };
+  
   outMapRev = new Map([...outMap.entries()].sort((a,b) => a[1] - b[1]));
   outMap = new Map([...outMap.entries()].sort((a,b) => b[1] - a[1]));
   outMapKeys = [...outMap.keys()].slice(0, 30);
   outMapValues = [...outMap.values()].slice(0, 30);
   outMapRevKeys = [...outMapRev.keys()].slice(0, 30);
   outMapRevValues = [...outMapRev.values()].slice(0, 30);
+  let blue = new Set(['(SBER', '(GAZP', '(LKOH', '(YNDX', '(GMKN', '(NVTK', '(POLY', '(ROSN', '(PLZL', '(MGNT', '(MTSS', '(TATN', '(MAIL', '(FIVE', '(SNGS']);
+  let myRe = /[(]\w+/;
+  
   for (let i = 0; i < namesHigh.length; i++) {
     namesHigh[i].textContent = outMapKeys[i];
+    if (blue.has(myRe.exec(namesHigh[i].textContent)[0])){
+      namesHigh[i].classList.add('bg-primary')};
     changesHigh[i].textContent = outMapValues[i];
     namesLow[i].textContent = outMapRevKeys[i];
+    if (blue.has(myRe.exec(namesLow[i].textContent)[0])){
+      namesLow[i].classList.add('bg-primary')};
     changesLow[i].textContent = outMapRevValues[i];
   };
 };
@@ -73,4 +94,5 @@ button.onclick = function () {
 window.onload = function(){
   button.click();
 }
+
 
