10a7045359a5ff36ef41c46f9e87803ae522bac8
ksn38 <ksn38@yandex.ru>
2021-03-19 16:03:25 +0300

    -

--- mybl/psql_req.py
+++ mybl/psql_req.py
@@ -1,12 +1,12 @@
 langs_today = '''select distinct id, name, val, val_noexp, res_vac, 
-(row_number() over(order by val desc) + row_number() over(order by val_noexp desc) + row_number() over(order by res_vac)) as rate
+(row_number() over(order by val desc) + rank() over(order by val_noexp desc) + row_number() over(order by res_vac)) as rate
 from mybl_lang ml where ml.date_added = current_date order by rate;'''
 
 chart_langs = '''select distinct b.id, a."name", ((b.val - a.aval)*100/a.aval) as cnd_val, ((b.val_noexp - a.aval_noexp)*100/a.aval_noexp) as cnd_vn, 
 ((b.res_vac - a.ares_vac)*100/a.ares_vac)::integer as cnd_rv,
-(row_number() over(order by  ((b.val - a.aval)*100/a.aval) desc) + 
-row_number() over(order by ((b.val_noexp - a.aval_noexp)*100/a.aval_noexp) desc) + 
-row_number() over(order by ((b.res_vac - a.ares_vac)*100/a.ares_vac)::integer)) as rate
+(rank() over(order by  ((b.val - a.aval)*100/a.aval) desc) + 
+rank() over(order by ((b.val_noexp - a.aval_noexp)*100/a.aval_noexp) desc) + 
+rank() over(order by ((b.res_vac - a.ares_vac)*100/a.ares_vac)::integer)) as rate
 from mean a
 left join  mybl_lang b on a."name"  = b."name" 
 where b.date_added = current_date order by rate;'''
